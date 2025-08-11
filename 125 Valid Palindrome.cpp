d186cac43750181df5513b08b23ff93f12663d52
j-anubhav <anubhavjha.dev@gmail.com>
2025-08-12 01:00:51 +0530

    used built in function for checking and got the result faster

--- 125 Valid Palindrome.cpp
+++ 125 Valid Palindrome.cpp
@@ -2,52 +2,30 @@
 #include<vector>
 using namespace std;
 
-
 class Solution {
 public:
-    vector<char> validString(string s){
-        vector<char> ans;
-        for(int i =0; i<s.size(); i++){
+    bool isPalindrome(string s) {
+        int left = 0;
+        int right = s.size() - 1;
 
-        if(s[i]>='a' && s[i]<='z' || s[i]>='0' && s[i]<='9'){
-           ans.push_back(s[i]);
-        }
-        }
-        return ans;
-    }
-    string isLowerCase(string s){
-        for(int i=0;i <s.size(); i++){
-            if(s[i]>='A' && s[i] <= 'Z'){
-                s[i] = s[i]-'A'+'a';
-
+        while(left <= right) {
+            if(!isalnum(s[left])) {
+                left++; 
+                continue;
             }
             
+            if(!isalnum(s[right])) {
+                right--;
+                continue;
+            }
+            
+            if(tolower(s[left]) != tolower(s[right])) {
+                return false;
+            } 
+                left++;
+                right--;
         }
-        
-        
-        return s;
-    }
-    bool isPalindrome(string s) {
-        
-      
 
-        string a = isLowerCase(s);
-        
-
-        vector<char> valid =validString(a);
-        
-        int start=0;
-        int end = valid.size()-1;
-        
-       while(start<end){
-        if(valid[start] == valid[end]){
-            end--;
-            start++;
-        }else{
-            return false;
-        }
-       }
-        
         return true;
     }
 };
